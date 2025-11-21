45eed5a803f444811c4c4846d617f49e6f3ec09e
osama <2023_osama.belaatar@iticbcn.cat>
2025-11-21 19:50:03 +0100

    avui estem fent registre_activity.xml y tot lo demes correcte

--- app/src/main/java/com/example/health_prototip/MainActivity.kt
+++ app/src/main/java/com/example/health_prototip/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.health_prototip
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var btn_iniciar_sesion : Button
+    private lateinit var nomLoginSesio: EditText
+    private lateinit var contrasenaLoginSesio: EditText
+    private lateinit var txt_registrarse: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,24 +24,28 @@ class MainActivity : AppCompatActivity() {
         initComponent()
         initListeners()
 
-
-
-
-
     }
-
     fun initComponent(){
         btn_iniciar_sesion = findViewById<Button>(R.id.btn_iniciar_sesion)
-
+        nomLoginSesio = findViewById(R.id.nomLoginSesio)
+        contrasenaLoginSesio = findViewById(R.id.ContraseñaLoginSesio)
+        txt_registrarse = findViewById(R.id.txt_registrarse)
     }
     fun initListeners(){
         btn_iniciar_sesion.setOnClickListener {
+            navigateToPaginaPrincipal_loby()
+        }
+        txt_registrarse.setOnClickListener {
             navigateToRegister()
         }
 
     }
     fun navigateToRegister(){
         val intent = Intent(this, registreActivity::class.java)
+        startActivity(intent)
+    }
+    fun navigateToPaginaPrincipal_loby(){
+        val intent = Intent(this, PaginaPrincipal_loby::class.java)
         startActivity(intent)
     }
 
---
+++ app/src/main/java/com/example/health_prototip/PaginaPrincipal_loby.kt
@@ -0,0 +1,16 @@
+package com.example.health_prototip
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PaginaPrincipal_loby : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_pagina_principal_loby)
+
+    }
+}
